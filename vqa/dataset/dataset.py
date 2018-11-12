9afb97b4791a63c9933f839c43a51cf3d59831e2
ram-iyer <ram.iyer.ca@gmail.com>
2018-11-12 20:28:40 +0000

    adding better baseline model. Adding hasattr check due to compile error

--- vqa/dataset/dataset.py
+++ vqa/dataset/dataset.py
@@ -561,7 +561,7 @@ class VQADataset:
         """Fits the tokenizer with the questions and answers and saves this tokenizer into a file for later use"""
 
         # contrary to the docs, `word_index` exists before training, so can't use `hasattr` check
-        if len(self.tokenizer.word_index) == 0:
+        if not hasattr(self.tokenizer,'word_index') :
             # only have to train it once.
             print('Tokenizer is not yet trained.  Training now...')
             questions_list = [question.question_str for _, question in questions.items()]
