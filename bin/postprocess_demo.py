c20dd0d8f171e7d0893a372f00e81468aa813494
Michael Winton <mwinton@berkeley.edu>
2018-11-19 01:38:47 +0000

    printing more descriptive text

--- bin/postprocess_demo.py
+++ bin/postprocess_demo.py
@@ -11,13 +11,12 @@ from vqa.dataset.dataset import VQADataset
 dataset_path = '/home/mwinton/w266-final-project/results/y_pred/y_proba_san_expt0_2018-11-18-23:50:13.p'
 with open(dataset_path, 'rb') as f:
     y_pred = pickle.load(f)
-print(y_pred.shape)
-print(y_pred[:20])
+print('Shape of retrieved y_pred:', y_pred.shape)
+print('Sample y_pred:', y_pred[:20])
 
 # determine predicted label
 y_argmax = np.argmax(y_pred, axis=1)
-print(y_argmax.shape)
-print(y_argmax)
+print('Sample predicted label indices (argmax):', y_argmax[:20])
 
 # load tokenizer to find corresponding words
 tokenizer_path = '/home/mwinton/w266-final-project/data/preprocessed/tokenizer.p'
@@ -29,8 +28,8 @@ index_to_word = dict([[v,k] for k, v in word_to_index.items()])
 index_to_word[0] = '<unk>'
 
 # determine corresponding text labels
-y_labels = [index_to_word[id] for _, id in enumerate(y_argmax)]
-print(y_labels[:20])
+y_pred_labels = [index_to_word[id] for _, id in enumerate(y_argmax)]
+print('sample predicted labels:', y_pred_labels[:20])
 
 # load validation dataset
 val_path =  '/home/mwinton/w266-final-project/data/preprocessed/validate_dataset.p'
@@ -39,6 +38,8 @@ val_dataset = pickle.load(open(val_path, 'rb'))
 # get sample.answer values from dataset
 # NOTE: these don't seem to be aligned with the y_pred results
 samples = val_dataset.samples
+print('dataset length (should match):', len(samples))
+print('sample true labels:')
 example = 0
 for s in samples:
     if example < 20:
